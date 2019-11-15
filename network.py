bd08509be653f86e9c434af953e2f7087ff9186d
neddstarkk <nedheesh.hasija@gmail.com>
2019-11-15 15:10:11 +0530

    Added OS detection and ping scanning functionality

--- network.py
+++ network.py
@@ -20,7 +20,7 @@ def portScan(host,port):
     nmapScan(tgtHost, str(tgtPort))
 
 def main():
-    print("What do you wanna do\n 1. Port Scan\n 2. Banner Grabbing\n")
+    print("What do you wanna do\n 1. Port Scan\n 2. Banner Grabbing\n 3. OS Detection\n 4. Ping Scan")
     a = int(input())
 
     if a == 1:
@@ -30,6 +30,13 @@ def main():
     elif a==2:
         host = input("Value for host: ")
         subprocess.call(['nmap', '-sV', '--script=banner ', host])
+    elif a == 3:
+        host = input("Value for host: ")
+        print ("You will need to provide administrative privileges")
+        subprocess.call(['sudo', 'nmap', '-O', host])
+    elif a == 4:
+        host = input("value for host: ")
+        subprocess.call(['ping', host])
 
 if __name__ == '__main__':
     main()
